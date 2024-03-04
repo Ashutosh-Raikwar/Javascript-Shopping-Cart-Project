d4ead185beaf04e34136d4f859740dada13d3df3
unknown <ashutoshraikwar225@gmail.com>
2024-03-04 18:34:27 +0530

    added a add to cart feature

--- script.js
+++ script.js
@@ -240,3 +240,35 @@ class Product{
         this.image = image;
     }
 }
+
+// getting and setting the items in localstorage
+
+class Storage {
+    static getproducts() {
+        let products;
+        if (localStorage.getItem("products")) {
+            products = JSON.parse(localStorage.getItem("products"));
+        } else {
+            products = [];
+        }
+        return products;
+    }
+
+    static addtolocalstorage(e) {
+        let products = Storage.getproducts();
+        products.push(e);
+        localStorage.setItem("products", JSON.stringify(products));
+    }
+
+    static removeproduct(id) {
+        let products = Storage.getproducts();
+        products.forEach((p, i) => {
+            if (p.id == id) {
+                products.splice(i, 1);
+            }
+        });
+        localStorage.setItem("products", JSON.stringify(products));
+    }
+}
+
+window.Storage = Storage;
